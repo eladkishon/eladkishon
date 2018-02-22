c6471846fb2f921f37edd7b1574059453b3bda49
Elad Keyshawn <elad.keyshawn@gmail.com>
2018-02-22 18:26:59 +0200

    remove console.logs

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,13 +1,10 @@
-import axios from "axios"
 
 export default {
 
     init: () => (state, actions) => {
-        console.log("initializing app state");
         const url = 'https://medium.com/feed/@eladk';
 
         feednami.load(url,(result) => {
-            console.log(result);
             if (result.error) {
                 console.log(result.error);
             } else {
