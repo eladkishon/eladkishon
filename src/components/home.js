399778d91131240c45531dff5c897ed795736562
eladkeyshawn <elad.keyshawn@gmail.com>
2019-02-16 20:45:33 +0200

    update

--- src/components/home.js
+++ src/components/home.js
@@ -7,28 +7,34 @@ export default ({posts}, children) =>
   <div>
 
       <div class="row buffer-top-half">
-          <div class="six columns">
+          <div class="twelve columns">
               <h1>Hey, I’m Elad. 👋</h1>
+              <h5>
+                  <a href="https://github.com/EladKeyshawn" target="_blank"> Github </a> |
+                  <a href="https://il.linkedin.com/in/elad-keyshawn-a68938119" target="_blank"> LinkedIn </a> |
+                  <a href="mailto:me@elad.636@gmail.com" target="_blank"> Email </a>
+                  {/*<a href="../../assets/files/resume.pdf" target="_blank"> Resume </a>*/}
+              </h5>
+
               <div class="thick-line"></div>
           </div>
       </div>
 
       <div class="row">
 
-          <div class="six columns">
+          <div class="twelve columns">
               {/*<Link to="/api">API</Link>*/}
-              <p> CS student, software engineer, cyclist and travel
+              <p> Software engineer, cyclist and travel
                   enthusiast.
                   <br/>
-                  Interested in deep learning, cryptography and articulated software architecture.
               </p>
 
           </div>
-
-          <div class="six columns">
+      </div>
+      <div className="row">
+      <div class="twelve columns">
               <h4>Posts</h4>
               {/*<div class="eight columns">*/}
-              {console.log(posts, children)}
               {
                   posts.map(post => {
                       return <div class="">
@@ -39,22 +45,19 @@ export default ({posts}, children) =>
               }
 
           </div>
-
       </div>
+
 
       <div class="row">
 
-          <div class="six columns">
+          <div class="twelve columns">
 
-              <p>You can find me on:</p>
-              <h5>
-                  <a href="https://github.com/EladKeyshawn" target="_blank"> Github </a> |
-                  <a href="https://il.linkedin.com/in/elad-keyshawn-a68938119" target="_blank"> LinkedIn </a> |
-                  <a href="mailto:me@elad.636@gmail.com" target="_blank"> Email </a> |
-                  <a href="../../assets/files/resume.pdf" target="_blank"> Resume </a>
-              </h5>
-
-              <span>pgp imprint: 8DD2 91AE 335D C758</span>
+              {/*<h5>*/}
+                  {/*<a href="https://github.com/EladKeyshawn" target="_blank"> Github </a> |*/}
+                  {/*<a href="https://il.linkedin.com/in/elad-keyshawn-a68938119" target="_blank"> LinkedIn </a> |*/}
+                  {/*<a href="mailto:me@elad.636@gmail.com" target="_blank"> Email </a> |*/}
+                  {/*<a href="../../assets/files/resume.pdf" target="_blank"> Resume </a>*/}
+              {/*</h5>*/}
 
 
           </div>
